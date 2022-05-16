fad0be8e870ec434c9c54fba8695b453558749ec
Minaeng <m271704@naver.com>
2022-05-16 12:46:58 +0900

    main page update complete

--- src/main/webapp/WEB-INF/views/index/index.jsp
+++ src/main/webapp/WEB-INF/views/index/index.jsp
@@ -31,7 +31,23 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <script  src="https://code.jquery.com/jquery-3.6.0.js" ></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/jquery.superslides.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/inewsticker.js"></script>
+    <script src="js/bootsnav.js"></script>
+    <script src="js/images-loded.min.js"></script>
+    <script src="js/isotope.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/baguetteBox.min.js"></script>
+    <script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
     <script type="text/javascript">
+    
 /* for문 사용 필요 */
 setInterval(function() {
         
@@ -368,19 +384,5 @@ setInterval(function() {
     </div>
   
     <!-- ALL JS FILES -->
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- ALL PLUGINS -->
-    <script src="js/jquery.superslides.min.js"></script>
-    <script src="js/bootstrap-select.js"></script>
-    <script src="js/inewsticker.js"></script>
-    <script src="js/bootsnav.js"></script>
-    <script src="js/images-loded.min.js"></script>
-    <script src="js/isotope.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/baguetteBox.min.js"></script>
-    <script src="js/form-validator.min.js"></script>
-    <script src="js/contact-form-script.js"></script>
-    <script src="js/custom.js"></script>
+   
 </body>
--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -1,20 +1,22 @@
 (function($) {
-    "use strict";
-	
-	/* ..............................................
-	   Loader 
-	   ................................................. */
+	"use strict";
+
+	/*
+	 * .............................................. Loader
+	 * .................................................
+	 */
 	$(window).on('load', function() {
 		$('.preloader').fadeOut();
 		$('#preloader').delay(550).fadeOut('slow');
 		$('body').delay(450).css({
-			'overflow': 'visible'
+			'overflow' : 'visible'
 		});
 	});
 
-	/* ..............................................
-	   Fixed Menu
-	   ................................................. */
+	/*
+	 * .............................................. Fixed Menu
+	 * .................................................
+	 */
 
 	$(window).on('scroll', function() {
 		if ($(window).scrollTop() > 50) {
@@ -24,22 +26,24 @@
 		}
 	});
 
-	/* ..............................................
-	   Gallery
-	   ................................................. */
+	/*
+	 * .............................................. Gallery
+	 * .................................................
+	 */
 
 	$('#slides-shop').superslides({
-		inherit_width_from: '.cover-slides',
-		inherit_height_from: '.cover-slides',
-		play: 5000,
-		animation: 'fade',
+		inherit_width_from : '.cover-slides',
+		inherit_height_from : '.cover-slides',
+		play : 5000,
+		animation : 'fade',
 	});
 
 	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
 
-	/* ..............................................
-	   Map Full
-	   ................................................. */
+	/*
+	 * .............................................. Map Full
+	 * .................................................
+	 */
 
 	$(document).ready(function() {
 		$(window).on('scroll', function() {
@@ -51,15 +55,16 @@
 		});
 		$('#back-to-top').click(function() {
 			$("html, body").animate({
-				scrollTop: 0
+				scrollTop : 0
 			}, 600);
 			return false;
 		});
 	});
 
-	/* ..............................................
-	   Special Menu
-	   ................................................. */
+	/*
+	 * .............................................. Special Menu
+	 * .................................................
+	 */
 
 	var Container = $('.container');
 	Container.imagesLoaded(function() {
@@ -68,106 +73,116 @@
 			$(this).addClass('active').siblings().removeClass('active');
 			var filterValue = $(this).attr('data-filter');
 			$grid.isotope({
-				filter: filterValue
+				filter : filterValue
 			});
 		});
 		var $grid = $('.special-list').isotope({
-			itemSelector: '.special-grid'
+			itemSelector : '.special-grid'
 		});
 	});
 
-	/* ..............................................
-	   BaguetteBox
-	   ................................................. */
+	/*
+	 * .............................................. BaguetteBox
+	 * .................................................
+	 */
 
 	baguetteBox.run('.tz-gallery', {
-		animation: 'fadeIn',
-		noScrollbars: true
+		animation : 'fadeIn',
+		noScrollbars : true
 	});
 
-	/* ..............................................
-	   Offer Box
-	   ................................................. */
+	/*
+	 * .............................................. Offer Box
+	 * .................................................
+	 */
 
 	$('.offer-box').inewsticker({
-		speed: 3000,
-		effect: 'fade',
-		dir: 'ltr',
-		font_size: 13,
-		color: '#ffffff',
-		font_family: 'Montserrat, sans-serif',
-		delay_after: 1000
+		speed : 3000,
+		effect : 'fade',
+		dir : 'ltr',
+		font_size : 13,
+		color : '#ffffff',
+		font_family : 'Montserrat, sans-serif',
+		delay_after : 1000
 	});
 
-	/* ..............................................
-	   Tooltip
-	   ................................................. */
+	/*
+	 * .............................................. Tooltip
+	 * .................................................
+	 */
 
 	$(document).ready(function() {
 		$('[data-toggle="tooltip"]').tooltip();
 	});
 
-	/* ..............................................
-	   Owl Carousel Instagram Feed
-	   ................................................. */
+	/*
+	 * .............................................. Owl Carousel Instagram
+	 * Feed .................................................
+	 */
 
-	$('.main-instagram').owlCarousel({
-		loop: true,
-		margin: 0,
-		dots: false,
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true,
-		navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
-		responsive: {
-			0: {
-				items: 2,
-				nav: true
-			},
-			600: {
-				items: 4,
-				nav: true
-			},
-			1000: {
-				items: 8,
-				nav: true,
-				loop: true
-			}
-		}
-	});
+	$('.main-instagram').owlCarousel(
+			{
+				loop : true,
+				margin : 0,
+				dots : false,
+				autoplay : true,
+				autoplayTimeout : 3000,
+				autoplayHoverPause : true,
+				navText : [ "<i class='fas fa-arrow-left'></i>",
+						"<i class='fas fa-arrow-right'></i>" ],
+				responsive : {
+					0 : {
+						items : 2,
+						nav : true
+					},
+					600 : {
+						items : 4,
+						nav : true
+					},
+					1000 : {
+						items : 8,
+						nav : true,
+						loop : true
+					}
+				}
+			});
 
-	/* ..............................................
-	   Featured Products
-	   ................................................. */
+	/*
+	 * .............................................. Featured Products
+	 * .................................................
+	 */
 
-	$('.featured-products-box').owlCarousel({
-		loop: true,
-		margin: 0,
-		dots: false,
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true,
-		navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
-		responsive: {
-			0: {
-				items: 1,
-				nav: true
-			},
-			600: {
-				items: 3,
-				nav: true
-			},
-			1000: {
-				items: 4,
-				nav: true,
-				loop: true
-			}
-		}
-	});
+	$('.featured-products-box').owlCarousel(
+			{
+				loop : true,
+				margin : 0,
+				dots : false,
+				autoplay : true,
+				autoplayTimeout : 3000,
+				autoplayHoverPause : true,
+				navText : [ "<i class='fas fa-arrow-left'></i>",
+						"<i class='fas fa-arrow-right'></i>" ],
+				responsive : {
+					0 : {
+						items : 1,
+						nav : true
+					},
+					600 : {
+						items : 3,
+						nav : true
+					},
+					1000 : {
+						items : 4,
+						nav : true,
+						loop : true
+					}
+				}
+			});
 
-	/* ..............................................
-	   Scroll
-	   ................................................. */
+	/*
+	 * .............................................. Scroll
+	 * .................................................
+	 */
 
 	$(document).ready(function() {
 		$(window).on('scroll', function() {
@@ -179,38 +194,40 @@
 		});
 		$('#back-to-top').click(function() {
 			$("html, body").animate({
-				scrollTop: 0
+				scrollTop : 0
 			}, 600);
 			return false;
 		});
 	});
 
-
-	/* ..............................................
-	   Slider Range
-	   ................................................. */
+	/*
+	 * .............................................. Slider Range
+	 * .................................................
+	 */
 
 	$(function() {
 		$("#slider-range").slider({
-			range: true,
-			min: 0,
-			max: 4000,
-			values: [1000, 3000],
-			slide: function(event, ui) {
+			range : true,
+			min : 0,
+			max : 4000,
+			values : [ 1000, 3000 ],
+			slide : function(event, ui) {
 				$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
 			}
 		});
-		$("#amount").val("$" + $("#slider-range").slider("values", 0) +
-			" - $" + $("#slider-range").slider("values", 1));
+		$("#amount").val(
+				"$" + $("#slider-range").slider("values", 0) + " - $"
+						+ $("#slider-range").slider("values", 1));
 	});
 
-	/* ..............................................
-	   NiceScroll
-	   ................................................. */
+	/*
+	 * .............................................. NiceScroll
+	 * .................................................
+	 */
+	$(function() {
+		$(".brand-box").niceScroll({
+			cursorcolor : "#9b9b9c",
+		});
+	})
 
-	$(".brand-box").niceScroll({
-		cursorcolor: "#9b9b9c",
-	});
-	
-	
 }(jQuery));
